634975064d976e2f62ff2c3b661ca226600ed9b7
Natalia Reyes <psic.nataliareyes@gmail.com>
2022-07-18 00:35:51 -0500

    se agrega primer bosquejo de vista con logo e imagen de fondo

---
+++ src/Components/HomeScreen/Home.js
@@ -0,0 +1,10 @@
+
+export default function Home(){
+  
+    return(
+        <div className="main-background">
+            <img className="main-logo" src="https://1000marcas.net/wp-content/uploads/2019/12/Harry-James-Potter-emblema.jpg" alt="logo"/>
+            <span className="filter-label">Selecciona tu Filtro</span>
+        </div>
+    )
+}
