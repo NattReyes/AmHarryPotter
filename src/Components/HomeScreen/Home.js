58e4e6713368a2671c81b782124adb96272947de
Natalia Reyes <psic.nataliareyes@gmail.com>
2022-07-18 13:34:33 -0500

    Se agrega estilos para la etiqueta de seleccionar filtro, tambien se modifica imagen del logo harry potter

--- src/Components/HomeScreen/Home.js
+++ src/Components/HomeScreen/Home.js
@@ -1,9 +1,9 @@
-
+import harry from '../img/harry.png'
 export default function Home(){
   
     return(
         <div className="main-background">
-            <img className="main-logo" src="https://1000marcas.net/wp-content/uploads/2019/12/Harry-James-Potter-emblema.jpg" alt="logo"/>
+            <img className="main-logo" src={harry} alt="logo"/>
             <span className="filter-label">Selecciona tu Filtro</span>
         </div>
     )
