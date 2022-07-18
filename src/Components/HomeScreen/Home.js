21bccda87d573216edf20a93482073c8b3ffe09c
Natalia Reyes <psic.nataliareyes@gmail.com>
2022-07-18 15:34:04 -0500

    Se agrega estilo y maquetado de los botones de filtrado

--- src/Components/HomeScreen/Home.js
+++ src/Components/HomeScreen/Home.js
@@ -7,6 +7,10 @@ export default function Home(){
              <NavBar/>
             <img className="main-logo" src={harry} alt="logo"/>
             <span className="filter-label">Selecciona tu Filtro</span>
+            <div className="button-div">
+                <button className="btn btn-filter" >ESTUDIANTES</button>
+                <button className="btn btn-filter" >STAFF</button>
+            </div>
         </div>
     )
 }
