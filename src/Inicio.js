4b0fd3174d0ce234cc50b1aa02a3dcf2470a514c
Natalia Reyes <psic.nataliareyes@gmail.com>
2022-07-17 19:27:36 -0500

    Se agrega instalación de react router dom, se modifica renderizado de app.js

---
+++ src/Inicio.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <div className='castleBackgrouns'>
+            <header className='headerButtons'>
+                hola harry
+            </header>
+            <div className="harryPotterLogo">
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
