3d35d1697d3beacf3b96c8ae7b503a0703f9c210
Natalia Reyes <psic.nataliareyes@gmail.com>
2022-07-19 18:04:16 -0500

    Se cambia la ruta de acceso del archivo styles.scss a la carpeta stylesHP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './styles.scss';
-
+//import './styles.scss';
+import '../src/Components/stylesHP/styles.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
