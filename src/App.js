8ddaa5e0d92944291317b37d9bdac22075c5d895
Natalia Reyes <psic.nataliareyes@gmail.com>
2022-07-20 12:55:20 -0500

    Se elimina el archivo manifiesto json para que se renderice la imagen de fondo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
   BrowserRouter, Routes, Route,
 } from "react-router-dom";
-import Home from './Components/HomeScreen/Home'
+import Home from './Components/HomeScreen/Home';
  
 function App() {
   
